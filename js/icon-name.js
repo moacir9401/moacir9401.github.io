502704273a00805fc036ac325cd55cd3de1b96c2
Moacir Afonso Alves <moacir9401@gmail.com>
2020-04-15 15:43:21 -0300

    inicial

---
+++ js/icon-name.js
@@ -0,0 +1,42 @@
+$('.icon-birds-heart').ready(function() {
+
+    imageSvg({ title: 'icon-birds-heart', icons: ['icon-small-heart', 'icon-birds', 'icon-leaf', 'icon-smallHeart02'] });
+})
+
+$('.icon-dance-couple').ready(function() {
+    imageSvg("icon-dance-couple");
+})
+
+$('.icon-romantic').ready(function() {
+    imageSvg("icon-romantic");
+})
+$('.icon-romatic02').ready(function() {
+    imageSvg("icon-romatic02");
+})
+$('.icon-kissing').ready(function() {
+    imageSvg("icon-kissing");
+})
+$('.icon-small-heart').ready(function() {
+
+    imageSvg('icon-small-heart');
+})
+$('.icon-birds').ready(function() {
+
+    imageSvg('icon-birds');
+})
+$('.icon-leaf').ready(function() {
+
+    imageSvg('icon-leaf');
+})
+$('.icon-smallHeart02').ready(function() {
+
+    imageSvg('icon-smallHeart02');
+})
+$('.icon-heart').ready(function() {
+
+    imageSvg('icon-heart');
+})
+$('.icon-sheets').ready(function() {
+
+    imageSvg('icon-sheets');
+})
